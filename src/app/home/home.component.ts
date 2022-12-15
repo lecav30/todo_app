054499d70f6326e87d9ebca3eed92491ace344e0
lecav30 <lecav30@outlook.com>
2022-12-14 20:04:06 -0500

    Sidevar v1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../service/todo/todo.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  projects: any = [];
 
-  constructor() { }
+  constructor(public todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.todoService.getProjects().subscribe((data) => {
+      this.projects = data;
+      console.log(this.projects);
+    });
   }
-
 }
